6c11b0f368c0090bf1181d0c29aa157e39d3c041
Garrett Heinlen <heinleng@gmail.com>
2016-12-08 13:28:32 +1100

    start of sending questions to the channel.

--- lib/fd_trivia/survey_says.ex
+++ lib/fd_trivia/survey_says.ex
@@ -10,13 +10,25 @@ defmodule FdTrivia.SurveySays do
   end
 
   def init(:ok) do
+    :timer.send_interval(10_000, :send_question)
     {:ok, %{count: 0, last_word: ""}}
   end
 
-  def handle_cast(%{"app" => "chat"} = msg, state) do
-    new_state = %{state | count: state.count + 1, last_word: msg["content"]}
-    IO.inspect(new_state)
-    {:noreply, new_state}
+  def handle_cast(%{"app" => "chat", "content" => content} = msg, state) do
+    cond do
+      # hack for the time being to ignore our own bot messages
+      String.starts_with?(content, "System-Question:") ->
+        {:noreply, state}
+      true ->
+        new_state = %{state | count: state.count + 1, last_word: content}
+        IO.inspect(new_state)
+        {:noreply, new_state}
+    end
+  end
+
+  def handle_info(:send_question, state) do
+    FlowdockClient.Sender.send_message("testttt", "System-Question: Who is the best?")
+    {:noreply, state}
   end
 
   def handle_cast(_, state) do
