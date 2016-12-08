8108ea8b7230e96806a1d8d62dcfb0c42295727e
Garrett Heinlen <heinleng@gmail.com>
2016-12-08 13:42:30 +1100

    hacking together some basic trivia question/answers

--- lib/fd_trivia/survey_says.ex
+++ lib/fd_trivia/survey_says.ex
@@ -1,6 +1,12 @@
 defmodule FdTrivia.SurveySays do
   use GenServer
 
+  @questions [
+    {"System-Question: Who is the best?", "Garrett"},
+    {"System-Question: Where am I?", "Work"},
+    {"System-Question: What's for dinner?", "Sandwich"},
+  ]
+
   def start_link do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
@@ -11,23 +17,35 @@ defmodule FdTrivia.SurveySays do
 
   def init(:ok) do
     :timer.send_interval(10_000, :send_question)
-    {:ok, %{count: 0, last_word: ""}}
+
+    state = %{
+      current_question: Enum.random(@questions)
+    }
+    {:ok, state}
   end
 
-  def handle_cast(%{"app" => "chat", "content" => content} = msg, state) do
+  def handle_cast(%{"app" => "chat", "content" => content} = msg, %{current_question: question} = state) do
     cond do
       # hack for the time being to ignore our own bot messages
       String.starts_with?(content, "System-Question:") ->
         {:noreply, state}
       true ->
-        new_state = %{state | count: state.count + 1, last_word: content}
-        IO.inspect(new_state)
-        {:noreply, new_state}
+        IO.inspect(elem(question, 1))
+        IO.inspect(content)
+
+        if elem(question, 1) == content do
+          FlowdockClient.Sender.send_message("testttt", "System-Question: Good job!")
+          new_state = %{state | current_question: Enum.random(@questions)}
+          IO.inspect(new_state)
+          {:noreply, new_state}
+        else
+          {:noreply, state}
+        end
     end
   end
 
-  def handle_info(:send_question, state) do
-    FlowdockClient.Sender.send_message("testttt", "System-Question: Who is the best?")
+  def handle_info(:send_question,  %{current_question: {question, answer}} = state) do
+    FlowdockClient.Sender.send_message("testttt", question)
     {:noreply, state}
   end
 
